033f11a7436f2c77b242d665b29b871498045f94
Bryan Hickey <bh@bigmanmedia.com.au>
2020-06-06 16:34:46 +1000

    added pinterest verfication to head

--- src/main.js
+++ src/main.js
@@ -3,8 +3,15 @@ import DefaultLayout from '~/layouts/Default.vue'
 export default function (Vue, { head }) {
   Vue.component('Layout', DefaultLayout)
 
+  // Add an external CSS file
   head.link.push({
     rel: 'stylesheet',
     href: 'https://fonts.googleapis.com/css?family=Mali:700|Nunito'
+  }),
+
+  // Add a meta tag
+  head.meta.push({
+    name: "p:domain_verify",
+    content: "32b98802270ed243841884e2ca3a9709"
   })
 }
